853db00bce74e58ecfacd290d94d81801aa95e17
Arloaitutor69 <arloaitutor@gmail.com>
2025-06-11 20:47:16 -0700

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -9,7 +9,7 @@ import openai
 import requests
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-CONTEXT_API = os.getenv("CONTEXT_API_BASE", "http://localhost:10000")
+CONTEXT_API = os.getenv("CONTEXT_API_BASE", "https://arlo-mvp-2.onrender.com")
 
 router = APIRouter()
 
